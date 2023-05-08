770986a842effce1f8aad9fc6eb9d3d8ab20d000
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2023-05-08 15:50:31 +0300

    made CLI app

---
+++ contacts.js
@@ -0,0 +1,49 @@
+const fs = require('fs').promises;
+const path = require('node:path');
+const { nanoid } = require('nanoid');
+
+const contactsPath = path.resolve('db', 'contacts.json');
+
+const updateContacts = async contacts => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, 'utf-8');
+  return JSON.parse(data);
+};
+
+const getContactById = async contactId => {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;
+};
+
+const removeContact = async contactId => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [results] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return results;
+};
+
+const addContact = async data => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
